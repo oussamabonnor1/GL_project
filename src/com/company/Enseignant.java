990e1bce9bbccb58e88288ecca1ea5155061489f
Oussama BONNOR <oussamabonnor@gmail.com>
2017-12-10 18:18:18 +0100

    Initial commit

---
+++ src/com/company/Enseignant.java
@@ -0,0 +1,17 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Enseignant {
+    private boolean responsableDepartement;
+    private String nom;
+    private String preNom;
+    private String tel;
+    private String mail;
+    private Date debutFonction;
+    private String grade;
+
+    private ArrayList<Matiere> matieres;
+
+}
---
+++ src/com/company/Etudiant.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Etudiant {
+    private Note note;
+    private ArrayList<Matiere> matieres;
+    private String nom;
+    private String preNom;
+    private String tel;
+    private String mail;
+    private Date debutFonction;
+    private String grade; //TODO: add a list of grades here
+    
+}
